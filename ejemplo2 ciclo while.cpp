1a532dba7811a223006059ce0f465057073e7bb6
Eli-3strada <125108357+Eli-3strada@users.noreply.github.com>
2023-02-23 21:14:20 -0600

    modificacion del ciclo while

--- ejemplo2 ciclo while.cpp
+++ ejemplo2 ciclo while.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
 	int a=1;
-	 
+	cout<<"Estos son los numeros del 1 al 100: "<<endl; 
 	while(a<=100)
 	{
-		cout<<"Estos son los numeros del 1 al 100: "<<endl;
-        cout<<a;
+		
+        cout<<a<<endl;
 	a=a+1;	
 	}
 	return 0;
