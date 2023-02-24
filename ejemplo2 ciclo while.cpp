9ccf6772759a20f77f07c9dbe1d0ce2b49a23452
Eli-3strada <125108357+Eli-3strada@users.noreply.github.com>
2023-02-23 20:25:52 -0600

    Cambio de ejercicio

---
+++ ejemplo2 ciclo while.cpp
@@ -0,0 +1,16 @@
+//Programa que imprima los numeros del 1 al 100
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=1;
+	 
+	while(a<=100)
+	{
+		cout<<"Estos son los numeros del 1 al 100: "<<endl;
+        cout<<a;
+	a=a+1;	
+	}
+	return 0;
+}
